a7a21b61b744e6de394eb790e87c0a41f22ae894
Stadzior <Stadzior@gmail.com>
2018-06-18 23:02:51 +0200

    Add project files.

---
+++ PathFindingBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PathFindingBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
